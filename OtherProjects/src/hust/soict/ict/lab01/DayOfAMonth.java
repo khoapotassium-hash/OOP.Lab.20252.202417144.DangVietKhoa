1eae91b91927958f41932bee85b44b1126f2c030
khoapotassium-hash <khoapotassium@gmail.com>
2026-04-19 02:48:01 +0700

    fix: Re-organize OtherProjects

--- OtherProjects/src/hust/soict/ict/lab01/DayOfAMonth.java
+++ OtherProjects/src/hust/soict/ict/lab01/DayOfAMonth.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class DaysOfAMonth {
+public class DayOfAMonth {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int month = -1;
