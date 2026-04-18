61ab69f3266ce5dca5265ffa563d4d25e39f22df
khoapotassium-hash <khoapotassium@gmail.com>
2026-04-19 00:00:15 +0700

    init: initialize CartTest

---
+++ AimsProject/AimsProject/src/CartTest.java
@@ -0,0 +1,23 @@
+public class CartTest {
+    public static void main(String[] args) {
+        Cart cart = new Cart();
+
+        DigitalVideoDisc dvd1 = new DigitalVideoDisc("The Lion King", "Animation", "Roger Allers", 87, 19.95f);
+        cart.addDigitalVideoDisc(dvd1);
+
+        DigitalVideoDisc dvd2 = new DigitalVideoDisc("Star Wars", "Science Fiction", "George Lucas", 87, 24.95f);
+        cart.addDigitalVideoDisc(dvd2);
+
+        DigitalVideoDisc dvd3 = new DigitalVideoDisc("Aladin", "Animation", 18.99f);
+        cart.addDigitalVideoDisc(dvd3);
+
+        // Test the print method
+        cart.print();
+
+        // Test the search methods 
+        System.out.println("\n--- Searching ---");
+        cart.searchById(2); 
+        cart.searchByTitle("Aladin"); 
+        cart.searchByTitle("Harry Potter"); 
+    }
+}
